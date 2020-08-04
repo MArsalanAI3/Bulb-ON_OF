01eb442f88bbaf6a4b07190118fd60b94b76d8c0
Muhammad Arsalan <marsalanai3@gmail.com>
2020-08-04 12:52:08 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+// var name=document.getElementById("name");
+
+// name.value="M.Arsalan";
+
+// function getvalue(){
+//    alert("test"); 
+// }
+
+// function setvalue(){
+//     var name =document.getElementById("name");
+
+//     name.value="basit";
+// }
+
+// function bigimage(){
+//    var para=document.getElementById("para");
+
+//    para.innerHTML="This is an example Text";
+// }
+
+
+// function bigimage(){
+//     var image=document.getElementById("img");
+
+//     image.className="big";
+// }
+
+// function smallimage(){
+//     var image=document.getElementById("img");
+
+//     image.className="small";
+// }
+
+function onbulb(){
+    var bulb=document.getElementById("bulb");
+
+    bulb.src ="images/bulb_On.jpg"
+    bulb.className ="ON";
+}
+function offbulb(){
+    var bulb=document.getElementById("bulb");
+
+    bulb.src ="images/bulb_Off.jpg"
+    bulb.className ="Off";
+}
+
+// function AddClass(){
+//     var para=document.getElementById("para").style.fontSize = "2em";;
+
+//     para.className +="bold blue";
+
+
+// }
+
+
